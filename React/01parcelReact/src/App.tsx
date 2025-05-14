e07f3290a8cd315ddd1bf9af7635e00b5a8a7b50
Cybersayak <sayakcse2@gmail.com>
2025-05-15 00:24:24 +0530

    REACT starter template by Parcel

---
+++ React/01parcelReact/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+export function App() {
+  return (
+    <>
+      <h1>Learning Namaste React by Akshay Saini </h1>
+      <h2>React Setup with Parcel Template </h2>
+    </>
+  );
+}
